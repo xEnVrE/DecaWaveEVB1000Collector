35a8f6c8dd6aff879549cbc719049e826e7820b1
xEnVrE <nicolapiga@gmail.com>
2017-10-18 23:38:39 +0200

    Remove GUI part to start transition to DecaWaveEVB1000Collector

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 # sys
 import sys
 
-# GUI
-from ui.gui import EVB1000ViewerMainWindow
-from PyQt5.QtWidgets import QApplication
-
 # DeviceManager
 from device.device_manager import DeviceManager
 from device.device_manager import DeviceVIDPIDList
@@ -17,19 +13,6 @@ if __name__ == '__main__':
     # instantiate device_manager
     dev_man = DeviceManager(vid_pid_list)
 
-    # instantiate a QApplication
-    app = QApplication(sys.argv)
-
-    # instantiate the main window
-    gui = EVB1000ViewerMainWindow(dev_man)
-
-    # show the main window
-    gui.show()
-
     # start the device manager
     dev_man.start()
-    
-    sys.exit(app.exec_())
-
-
     
